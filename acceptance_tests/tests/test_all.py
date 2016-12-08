d7a42be05bff992bed0ca1ca64496289c511be4b
Jakub Skiepko <jakub.skiepko@socialwifi.com>
2016-12-08 18:30:48 +0100

    Add more acceptance tests.

--- acceptance_tests/tests/test_all.py
+++ acceptance_tests/tests/test_all.py
@@ -10,4 +10,16 @@ def test_first(selenium: selenium.webdriver.Remote, running_server_url):
     file_upload.send_keys(str(pathlib.Path(__file__).parent.parent / 'test.po'))
     selenium.find_element_by_css_selector('input[type="submit"]').click()
     selenium.implicitly_wait(2)
-    assert 'File uploaded' in selenium.find_element_by_tag_name('body').text
+    content = selenium.find_element_by_tag_name('body').text
+    assert 'File uploaded' in content
+    assert 'Disambiguation for context' in content
+    assert 'Een' in content
+    selenium.find_element_by_link_text('One').click()
+    text_input = selenium.find_element_by_css_selector('input[type="text"]')
+    text_input.clear()
+    text_input.send_keys('New translation')
+    selenium.find_element_by_css_selector('input[type="submit"]').click()
+    content = selenium.find_element_by_tag_name('body').text
+    assert 'File uploaded' in content
+    assert 'Disambiguation for context' in content
+    assert 'New translation' in content
