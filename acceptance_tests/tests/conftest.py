a18bc0abe2e5a74a6e9083de845c6791e4429a1c
Jakub Skiepko <jakub.skiepko@socialwifi.com>
2017-03-07 17:49:28 +0100

    Add postgres.

--- acceptance_tests/tests/conftest.py
+++ acceptance_tests/tests/conftest.py
@@ -17,11 +17,22 @@ def docker_image():
     build(image_name)
     yield image_name
 
+@pytest.fixture(scope="session")
+def postgres_server():
+    container_name = 'acceptance_test_dila_postgres'
+    sh.docker('run', '-d', '--name', container_name, 'postgres')
+    log = sh.docker('logs', '-f', container_name, _iter=True, _ok_code=2)
+    for line in log:
+        if 'PostgreSQL init process complete; ready for start up.' in line:
+            break
+    log.terminate()
+    yield container_name
+    sh.docker('rm', '-fv', container_name)
 
 @pytest.fixture(scope="session")
-def running_server(docker_image):
+def running_server(postgres_server, docker_image):
     container_name = 'test_dila'
-    sh.docker('run', '-d', '--name', container_name, docker_image)
+    sh.docker('run', '-d', '--name', container_name, '--link', '{}:db'.format(postgres_server), docker_image)
     log = sh.docker('logs', '-f', container_name, _iter='err', _ok_code=2)
     for line in log:
         if 'Running on http://0.0.0.0:80/' in line:
