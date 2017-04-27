eee44af6ca9cba1e65b4b9647ca1aa6918cbb3f7
Jakub Skiepko <jakub.skiepko@socialwifi.com>
2017-04-27 17:14:08 +0200

    Back to development: 0.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,12 @@
 Changelog for dila
 =================
 
+0.0.1 (unreleased)
+------------------
+
+- Nothing changed yet.
+
+
 0.0.0 (2017-04-27)
 ------------------
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='dila',
-    version='0.0.0',
+    version='0.0.1.dev0',
     description='Dila is a open source web-based translation platform for translators, content creators and developers.',
     author='Jakub Skiepko',
     author_email='it@socialwifi.com',
